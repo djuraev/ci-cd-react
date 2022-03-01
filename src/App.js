f257262e8aa15c4857fee48770b2b01f17d76d5a
Djuraev Mamurjon <mamurjon@nextree.io>
2022-03-01 12:08:16 +0500

    add new feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import { Button } from '@mui/material';
+import {Button, ButtonGroup} from '@mui/material';
 
 
 function App() {
   return (
+      <ButtonGroup>
+
     <Button>Hello World guys</Button>
+    <Button>Hello World Girls</Button>
+    <Button>Hello World Kids</Button>
+    <Button>Hello World Other</Button>
+
+      </ButtonGroup>
   );
 }
 
